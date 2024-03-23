dcb20098eda036a0a4141ad3749bff6435339056
richardaniel <richarlopez102002@gmail.com>
2024-03-22 21:03:28 -0600

    Conexion con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:examen2_grupo4/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:examen2_grupo4/core/router/app_router.dart';
 import 'package:examen2_grupo4/presentation/screens/home/home_screen.dart';
@@ -5,7 +7,9 @@ import 'package:examen2_grupo4/presentation/screens/ver/ver_vehiculos_screen.dar
 import 'package:examen2_grupo4/presentation/screens/agregar/agregar_vehiculo_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/buscar/buscar_vehiculo_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MainApp());
 }
 
