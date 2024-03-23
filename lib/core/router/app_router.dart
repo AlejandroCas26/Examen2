5941663ef76d76d8502dce7c23ed7ebc7ec256e9
richardaniel <richarlopez102002@gmail.com>
2024-03-22 21:57:00 -0600

    update

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,26 +1,26 @@
 import 'package:examen2_grupo4/presentation/screens/home/home_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/ver/ver_vehiculos_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/buscar/buscar_vehiculo_screen.dart';
-import 'package:examen2_grupo4/presentation/screens/borrar/borrar_screen.dart'; 
+import 'package:examen2_grupo4/presentation/screens/borrar/borrar_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => HomeScreen(),
+      builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/ver_vehiculos',
-      builder: (context, state) => VerVehiculosScreen(),
+      builder: (context, state) => const VerVehiculosScreen(),
     ),
     GoRoute(
       path: '/buscar_vehiculo',
-      builder: (context, state) => BuscarVehiculoScreen(),
+      builder: (context, state) => const BuscarVehiculoScreen(),
     ),
     GoRoute(
-      path: '/borrar_vehiculo', 
-      builder: (context, state) => BorrarVehiculoScreen(),
+      path: '/borrar_vehiculo',
+      builder: (context, state) => const BorrarVehiculoScreen(),
     ),
   ],
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:examen2_grupo4/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:examen2_grupo4/core/router/app_router.dart';
@@ -6,16 +5,15 @@ import 'package:examen2_grupo4/presentation/screens/home/home_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/ver/ver_vehiculos_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/agregar/agregar_vehiculo_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/buscar/buscar_vehiculo_screen.dart';
-import 'package:examen2_grupo4/presentation/screens/borrar/borrar_screen.dart'; 
+import 'package:examen2_grupo4/presentation/screens/borrar/borrar_screen.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MainApp());
+void main() {
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,10 +21,10 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Autos',
       routes: {
-        '/': (context) => HomeScreen(),
-        '/ver_vehiculos': (context) => VerVehiculosScreen(),
-        '/buscar_vehiculo': (context) => BuscarVehiculoScreen(),
-        '/borrar_vehiculo': (context) => BorrarVehiculoScreen(),
+        '/': (context) => const HomeScreen(),
+        '/ver_vehiculos': (context) => const VerVehiculosScreen(),
+        '/buscar_vehiculo': (context) => const BuscarVehiculoScreen(),
+        '/borrar_vehiculo': (context) => const BorrarVehiculoScreen(),
       },
     );
   }
