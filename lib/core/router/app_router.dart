13cd6cc5aba736f2b5fe566dd7a75a88b4910521
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-03-22 20:59:30 -0600

    update
    Agregando el boton borrar al home y configurandolo en go router y main.

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:examen2_grupo4/presentation/screens/home/home_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/ver/ver_vehiculos_screen.dart';
-import 'package:examen2_grupo4/presentation/screens/agregar/agregar_vehiculo_screen.dart';
 import 'package:examen2_grupo4/presentation/screens/buscar/buscar_vehiculo_screen.dart';
+import 'package:examen2_grupo4/presentation/screens/borrar/borrar_screen.dart'; 
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
@@ -14,10 +14,13 @@ final GoRouter router = GoRouter(
       path: '/ver_vehiculos',
       builder: (context, state) => VerVehiculosScreen(),
     ),
-   
     GoRoute(
       path: '/buscar_vehiculo',
       builder: (context, state) => BuscarVehiculoScreen(),
+    ),
+    GoRoute(
+      path: '/borrar_vehiculo', 
+      builder: (context, state) => BorrarVehiculoScreen(),
     ),
   ],
 );
